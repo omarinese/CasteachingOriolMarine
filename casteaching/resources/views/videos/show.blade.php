6df8e2d848abb63ad836494aab7c151b436d6f05
omarine12 <omarine@iesebre.com>
2024-03-09 20:16:45 +0100

    Video 10 acabat

--- casteaching/resources/views/videos/show.blade.php
+++ casteaching/resources/views/videos/show.blade.php
@@ -1,9 +1,24 @@
 <x-casteaching-layout>
-    <p>{{$video->title}}</p>
+    <div class="flex flex-col">
+        <iframe
+            class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10"
+            height="600"
+            src="https://www.youtube.com/embed/kzlB7dvemfM?si=DqjzxTk__SeSLdW6"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
 
-    <ul>
-        <li>Description: {{$video->description}}</li>
-        <li>Data: {{$video->published_at}}</li>
-    </ul>
+        <div class="p-4 lg:p-5 xl:p-10 2xl:p-20">
+            {{$video->title}}
+        </div>
+
+        <div>
+            {{$video->description}}
+        </div>
+
+        @if ($video->user)
+            <h3>{{$video->user->name}}</h3>
+        @endif
+
+    </div>
 </x-casteaching-layout>
-
